2c354e2559ca7a3f96ec6c66b9492d6b4ede36a7
hanh <hanhhaohoa11a13@gmail.com>
2023-10-14 23:30:15 +0700

    first commit

---
+++ electricity-backend/src/main/java/com/example/electricitybackend/ElectricityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.electricitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricityBackendApplication.class, args);
+    }
+
+}
---
+++ electricity-backend/src/test/java/com/example/electricitybackend/ElectricityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electricitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
